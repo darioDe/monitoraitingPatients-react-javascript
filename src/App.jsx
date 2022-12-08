01062f51625b3270778b9f62c378f85538e5bd8b
Ruben Duarte <rdduarte1811@gmail.com>
2022-12-08 08:28:50 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   useEffect (() => {
     const getLS = () => {
       const infoPatientLS = JSON.parse(localStorage.getItem('infoPatient')) ?? [];
-      console.log(infoPatientLS)
+      // console.log(infoPatientLS)CD
       setInfoPatient(infoPatientLS);
     }
 
